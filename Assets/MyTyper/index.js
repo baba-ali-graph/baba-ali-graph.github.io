1aa2f8426544458b197e2f7704bcdfa875495212
Baba Ali <babaali196@gmail.com>
2019-07-01 10:46:27 +0100

    typewriter effect added

---
+++ Assets/MyTyper/index.js
@@ -0,0 +1,25 @@
+		
+			function createTyper(str, identifier,delayTime,looped,blinker,spd) {
+			const words = str.split('|')
+			const options = createOptions(words,delayTime,looped,blinker,spd)
+			const toAttach = createElemAndAttachToParent(identifier)
+			const myTyper = new Typed(toAttach,options)
+			return myTyper
+			}
+			
+			const createOptions = (words,delayed,looped,blinker,spd) => ({
+				strings : words,
+				typeSpeed : 20,
+				backSpeed : 1,
+				showCursor : blinker ? blinker : true,
+				startDelay : delayed ? delayed : 0,
+				loop:looped ? looped : true
+			})
+			
+			function createElemAndAttachToParent(identifier){
+				const elem = document.createElement('span')
+				const parent = document.querySelector(identifier)
+				parent.appendChild(elem)
+				elem.setAttribute('id',''+identifier.split(/[.#]/)[1]+'-child')
+				return "#" + elem.getAttribute('id')
+			}
